b364aa30f16c96bd37b4a80671a717d8d03ea7d4
acairns1984 <alex.p.cairns@gmail.com>
2016-11-17 12:26:18 -0500

    First Commit

---
+++ Assignment Code.R
@@ -0,0 +1,88 @@
+#-----------------------------
+#-----------------------------
+#--- Pratical Machine Learning - Course Project
+
+remove(list=ls())
+library(caret)
+library(e1071)
+library(gbm)
+library(randomForest)
+library(ggplot2)
+
+#------------------
+#-- Import Data
+
+#Load the data
+setwd("U:/Coursera/Practical Machine Learning/Week 4 Project/")
+train.data <- read.csv(file = "pml-training.csv", header = TRUE)
+
+#------------------
+#-- Clean Data
+
+#Eliminate observations where "New_window" equals yes
+summary(train.data[train.data$new_window == "no",])
+train.data <- train.data[train.data$new_window == "no",]
+
+#Eliminate variables where all observations are NAs
+NAObs <- apply(train.data, 2, function(i) sum(!is.na(i)) == nrow(train.data))
+train.data <- train.data[,names(NAObs[NAObs == TRUE])]
+
+#Eliminate variables where all observations are NAs
+CompleteObs <- apply(train.data, 2, function(i) length(i[i == ""]) == 0)
+train.data <- train.data[, names(CompleteObs[CompleteObs == TRUE])]
+
+#Drop Unneeded columns
+ColToDelete <- c("X", "user_name", "new_window", "raw_timestamp", "num_window", "cvtd_timestamp")
+train.data <- train.data[, -grep(paste(ColToDelete, collapse = "|"), colnames(train.data))]
+
+#------------------
+#-- Identify predictors 
+
+### Model 1
+
+fit.rf <- randomForest(classe ~ ., data = train.data, ntree = 500, mtry = 7, importance = T)
+
+varImpPlot(fit.rf)
+
+varimp <- importance(fit.rf, type = 1) #mean decrease in node purity
+RankVarImp <- as.matrix(cbind(varimp[order(-varimp[,1]), ], Rank = 1:nrow(varimp)))
+
+### Loop Through various specifications
+
+Iterations <- lapply(7:nrow(RankVarImp), function(i){
+            print(i)
+            varNames <- rownames(RankVarImp[1:i, ])
+            fit <- randomForest(train.data[,varNames], y = train.data$classe, ntree = 500, mtry = 7)
+            k <- fit$confusion[,1:5]
+            OOBestimate <- 1 - sum(diag(k))/sum(colSums(k))
+            OOBestimate
+      })
+
+dat <- data.frame(NumofVar = 7:nrow(RankVarImp), OOBError = do.call(c, Iterations))
+
+g <- ggplot(data = dat, aes(x = NumofVar, y = OOBError)) + geom_line(col = "blue")
+g <- g + ggtitle("OOB Error by Variable Count")
+g <- g + vline(dat[dat$OOBError == min(dat[,"OOBError"]), "NumofVar"])
+g
+
+### Final Model 2
+
+varnames <- rownames(RankVarImp[1:50,])
+fit.rfFinal <- randomForest(classe ~ roll_belt + yaw_belt + pitch_forearm + magnet_dumbbell_z + pitch_belt + magnet_dumbbell_y
+                            + roll_forearm + magnet_dumbbell_x + roll_dumbbell + accel_dumbbell_y + accel_belt_z
+                            + magnet_belt_y + magnet_belt_z + accel_forearm_x + accel_dumbbell_z + roll_arm + gyros_belt_z
+                            + magnet_forearm_z + total_accel_dumbbell + yaw_dumbbell + magnet_arm_x + yaw_arm + gyros_dumbbell_y
+                            + magnet_belt_x + accel_forearm_z + accel_dumbbell_x + magnet_arm_y + magnet_forearm_y + magnet_forearm_x
+                            + accel_arm_x + total_accel_belt + yaw_forearm + magnet_arm_z + pitch_arm + accel_arm_y
+                            + accel_forearm_y + gyros_arm_y + gyros_arm_x + accel_arm_z + gyros_dumbbell_x + gyros_forearm_y
+                            + gyros_belt_y + accel_belt_x + total_accel_forearm + total_accel_arm + gyros_belt_x
+                            + gyros_dumbbell_z +  gyros_forearm_z + gyros_forearm_x + gyros_arm_z
+                            , data = train.data, ntree = 2500, mtry = 7, importance = T)
+#fit.rfFinal
+
+### Make Predictions
+
+test.data <- read.csv(file = "pml-testing.csv", header = TRUE)
+predict(fit.rfFinal, newdata = test.data)
+
+?predict
