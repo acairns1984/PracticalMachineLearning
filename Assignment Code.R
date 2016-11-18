d4e61118a890ff4b471efeeaa002220c2260d868
acairns1984 <alex.p.cairns@gmail.com>
2016-11-17 21:46:24 -0500

    Second Commit

--- Assignment Code.R
+++ Assignment Code.R
@@ -40,34 +40,40 @@ train.data <- train.data[, -grep(paste(ColToDelete, collapse = "|"), colnames(tr
 
 ### Model 1
 
-fit.rf <- randomForest(classe ~ ., data = train.data, ntree = 500, mtry = 7, importance = T)
+set.seed(123)
+fit.rf <- randomForest(classe ~ ., data = train.data, ntree = 50, mtry = 7, importance = T)
 
-varImpPlot(fit.rf)
-
-varimp <- importance(fit.rf, type = 1) #mean decrease in node purity
+varimp <- importance(fit.rf, type = 2) #mean decrease in node purity
 RankVarImp <- as.matrix(cbind(varimp[order(-varimp[,1]), ], Rank = 1:nrow(varimp)))
+RankVarImp
+
+varImpPlot(fit.rf, main = "Figure 1: Variable Importance")
+
 
 ### Loop Through various specifications
 
 Iterations <- lapply(7:nrow(RankVarImp), function(i){
             print(i)
             varNames <- rownames(RankVarImp[1:i, ])
+            set.seed(i)
             fit <- randomForest(train.data[,varNames], y = train.data$classe, ntree = 500, mtry = 7)
             k <- fit$confusion[,1:5]
             OOBestimate <- 1 - sum(diag(k))/sum(colSums(k))
             OOBestimate
       })
 
+# Variable count with lowest OOB error
 dat <- data.frame(NumofVar = 7:nrow(RankVarImp), OOBError = do.call(c, Iterations))
+dat[dat$OOBError == min(dat$OOBError),]
 
+# Plot
 g <- ggplot(data = dat, aes(x = NumofVar, y = OOBError)) + geom_line(col = "blue")
-g <- g + ggtitle("OOB Error by Variable Count")
-g <- g + vline(dat[dat$OOBError == min(dat[,"OOBError"]), "NumofVar"])
-g
+g <- g + ggtitle("Figure 2: OOB Error by Variable Count") + xlab("Number of Predictors Included (According to Rank)")
+g + ylab("OOB Error (1 - accuracy)")
 
-### Final Model 2
+### Final Model
 
-varnames <- rownames(RankVarImp[1:50,])
+set.seed(12345)
 fit.rfFinal <- randomForest(classe ~ roll_belt + yaw_belt + pitch_forearm + magnet_dumbbell_z + pitch_belt + magnet_dumbbell_y
                             + roll_forearm + magnet_dumbbell_x + roll_dumbbell + accel_dumbbell_y + accel_belt_z
                             + magnet_belt_y + magnet_belt_z + accel_forearm_x + accel_dumbbell_z + roll_arm + gyros_belt_z
@@ -76,13 +82,17 @@ fit.rfFinal <- randomForest(classe ~ roll_belt + yaw_belt + pitch_forearm + magn
                             + accel_arm_x + total_accel_belt + yaw_forearm + magnet_arm_z + pitch_arm + accel_arm_y
                             + accel_forearm_y + gyros_arm_y + gyros_arm_x + accel_arm_z + gyros_dumbbell_x + gyros_forearm_y
                             + gyros_belt_y + accel_belt_x + total_accel_forearm + total_accel_arm + gyros_belt_x
-                            + gyros_dumbbell_z +  gyros_forearm_z + gyros_forearm_x + gyros_arm_z
+                            + gyros_dumbbell_z +  pitch_dumbbell + accel_belt_y
                             , data = train.data, ntree = 2500, mtry = 7, importance = T)
-#fit.rfFinal
 
-### Make Predictions
+#Out of sample/OOB Error of Final Model
+fit.rfFinal
 
+### Make Test Set Predictions
 test.data <- read.csv(file = "pml-testing.csv", header = TRUE)
-predict(fit.rfFinal, newdata = test.data)
+pred.val <- predict(fit.rfFinal, newdata = test.data)
+pred.val
 
-?predict
+
+
+
